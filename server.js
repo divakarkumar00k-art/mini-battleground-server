8f5889c08357c6667652413a6b20719ea9c51444
divakarkumar00k-art <divakarkumar00k@gmail.com>
2026-02-17 10:54:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,58 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (ws) => {
-  console.log("Player connected");
+let players = {};
 
-  ws.send("Welcome to Mini Battleground Server");
+function generateId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+function broadcast(data) {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+wss.on("connection", (ws) => {
+  const playerId = generateId();
+  players[playerId] = { id: playerId };
+
+  console.log("Player connected:", playerId);
+
+  ws.send(JSON.stringify({
+    type: "welcome",
+    playerId: playerId
+  }));
+
+  broadcast({
+    type: "playerCount",
+    total: Object.keys(players).length
+  });
 
   ws.on("message", (message) => {
-    console.log("Received:", message.toString());
+    try {
+      const data = JSON.parse(message);
+
+      if (data.type === "join") {
+        players[playerId].username = data.username;
+        console.log("Player joined as:", data.username);
+      }
+
+    } catch (err) {
+      console.log("Invalid message received");
+    }
   });
 
   ws.on("close", () => {
-    console.log("Player disconnected");
+    delete players[playerId];
+    console.log("Player disconnected:", playerId);
+
+    broadcast({
+      type: "playerCount",
+      total: Object.keys(players).length
+    });
   });
 });
 
