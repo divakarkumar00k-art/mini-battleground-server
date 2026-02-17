731a9d373ac4e43c1a319f02dd79a6bf0d3394eb
divakarkumar00k-art <divakarkumar00k@gmail.com>
2026-02-17 12:35:07 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,1 +1,19 @@
+import LoginScene from "./scenes/LoginScene.js";
+import LobbyScene from "./scenes/LobbyScene.js";
+import MatchScene from "./scenes/MatchScene.js";
 
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: "#000000",
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  scene: [LoginScene, LobbyScene, MatchScene]
+};
+
+new Phaser.Game(config);
