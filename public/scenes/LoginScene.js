4db22a8a43c77b3bcd69b61fc6c34f29c7c260c1
divakarkumar00k-art <divakarkumar00k@gmail.com>
2026-02-17 12:35:49 +0530

    Update LoginScene.js

--- public/scenes/LoginScene.js
+++ public/scenes/LoginScene.js
@@ -1,1 +1,23 @@
+export default class LoginScene extends Phaser.Scene {
+  constructor() {
+    super("LoginScene");
+  }
 
+  create() {
+    this.add.text(300, 200, "Mini Battleground", {
+      fontSize: "40px",
+      fill: "#ffffff"
+    });
+
+    const loginBtn = this.add.text(350, 350, "ENTER GAME", {
+      fontSize: "30px",
+      fill: "#00ff00",
+      backgroundColor: "#222",
+      padding: 10
+    }).setInteractive();
+
+    loginBtn.on("pointerdown", () => {
+      this.scene.start("LobbyScene");
+    });
+  }
+}
