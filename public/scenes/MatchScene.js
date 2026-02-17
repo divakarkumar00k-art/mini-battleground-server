2731da15851e97b4b243c2519305ab3e0f64e5d5
divakarkumar00k-art <divakarkumar00k@gmail.com>
2026-02-17 12:36:46 +0530

    Update MatchScene.js

--- public/scenes/MatchScene.js
+++ public/scenes/MatchScene.js
@@ -1,1 +1,17 @@
+export default class MatchScene extends Phaser.Scene {
+  constructor() {
+    super("MatchScene");
+  }
 
+  create() {
+    this.add.text(300, 200, "Match Started", {
+      fontSize: "40px",
+      fill: "#ffffff"
+    });
+
+    this.add.text(250, 300, "Aircraft Jump System Coming Next 🚀", {
+      fontSize: "25px",
+      fill: "#ffff00"
+    });
+  }
+}
