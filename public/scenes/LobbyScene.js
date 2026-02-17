f36b4ab4888c4fc61f5cd11942583cb86af8000b
divakarkumar00k-art <divakarkumar00k@gmail.com>
2026-02-17 12:36:17 +0530

    Update LobbyScene.js

--- public/scenes/LobbyScene.js
+++ public/scenes/LobbyScene.js
@@ -1,1 +1,23 @@
+export default class LobbyScene extends Phaser.Scene {
+  constructor() {
+    super("LobbyScene");
+  }
 
+  create() {
+    this.add.text(300, 200, "Lobby", {
+      fontSize: "40px",
+      fill: "#ffffff"
+    });
+
+    const startBtn = this.add.text(350, 350, "START MATCH", {
+      fontSize: "30px",
+      fill: "#ff0000",
+      backgroundColor: "#222",
+      padding: 10
+    }).setInteractive();
+
+    startBtn.on("pointerdown", () => {
+      this.scene.start("MatchScene");
+    });
+  }
+}
